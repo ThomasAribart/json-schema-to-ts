24daaf436308581f86f89c09eb4a5a2640cae1bd
ThomasAribart <thomasa@theodo.fr>
2020-07-14 21:47:13 +0200

    return never if const doesn't match type

--- src/const.ts
+++ src/const.ts
@@ -2,6 +2,8 @@ import { FromWriteableSchema } from "./index";
 
 export type FromConstSchema<S> = "const" extends keyof S
   ? "type" extends keyof S
-    ? S["const"] & FromWriteableSchema<Omit<S, "const" | "enum">>
+    ? S["const"] extends FromWriteableSchema<Omit<S, "const" | "enum">>
+      ? S["const"]
+      : never
     : S["const"]
   : never;
