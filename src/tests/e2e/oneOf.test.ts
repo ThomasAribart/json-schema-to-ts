5f8c738fc730eeff02adb634d8e8e1cdacd71663
ThomasAribart <thomasa@theodo.fr>
2020-11-28 12:09:24 +0100

    improve & test handling of tuple properties in oneOf shemas

--- src/tests/e2e/oneOf.test.ts
+++ src/tests/e2e/oneOf.test.ts
@@ -119,6 +119,34 @@ describe("OneOf schemas", () => {
       });
     });
 
+    describe("Open to open object (impossible)", () => {
+      const factoredObjSchema = {
+        type: "object",
+        properties: { str: { type: "string" } },
+        required: ["str"],
+        oneOf: [{ additionalProperties: { type: "boolean" } }],
+      } as const;
+
+      type FactoredObj = FromSchema<typeof factoredObjSchema>;
+      let factoredObjInstance: FactoredObj;
+
+      it("rejects object not matching child", () => {
+        // @ts-expect-error
+        factoredObjInstance = { str: "str" };
+        expect(ajv.validate(factoredObjSchema, factoredObjInstance)).toBe(
+          false
+        );
+      });
+
+      it("rejects objects not matching parent", () => {
+        // @ts-expect-error
+        factoredObjInstance = { str: true };
+        expect(ajv.validate(factoredObjSchema, factoredObjInstance)).toBe(
+          false
+        );
+      });
+    });
+
     describe("Closed (1) to open object", () => {
       const factoredObjSchema = {
         type: "object",
@@ -433,5 +461,444 @@ describe("OneOf schemas", () => {
       });
     });
   });
-  // TODO: Test tuples
+
+  describe("Factored tuple properties", () => {
+    describe("Open tuples", () => {
+      const tupleSchema = {
+        type: "array",
+        items: [{ type: "string" }],
+        oneOf: [
+          { items: [{ const: "num" }, { type: "number" }] },
+          { items: [{ const: "bool" }, { type: "boolean" }] },
+        ],
+      } as const;
+
+      type Tuple = FromSchema<typeof tupleSchema>;
+      let tupleInstance: Tuple;
+
+      it("accepts tuples matching #1", () => {
+        tupleInstance = ["num"];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(true);
+
+        tupleInstance = ["num", 42];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(true);
+      });
+
+      it("accepts tuples matching #2", () => {
+        tupleInstance = ["bool"];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(true);
+
+        tupleInstance = ["bool", true];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(true);
+      });
+
+      it("rejects tuples matching neither", () => {
+        // @ts-expect-error: First item should be "num"/"bool"
+        tupleInstance = ["not num/bool"];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(false);
+
+        // @ts-expect-error: Second item should be number
+        tupleInstance = ["num", true];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(false);
+
+        // @ts-expect-error: Second item should be bool
+        tupleInstance = ["bool", 42];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(false);
+      });
+    });
+
+    describe("Open tuples (2)", () => {
+      const tupleSchema = {
+        type: "array",
+        items: [{ type: "string" }],
+        additionalItems: { type: "boolean" },
+        oneOf: [
+          { items: [{ const: "num" }, { type: "number" }] },
+          { items: [{ const: "bool" }, { type: "boolean" }] },
+        ],
+      } as const;
+
+      type Tuple = FromSchema<typeof tupleSchema>;
+      let tupleInstance: Tuple;
+
+      it("accepts tuples matching #1", () => {
+        tupleInstance = ["num"];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(true);
+      });
+
+      it("accepts tuples matching #2", () => {
+        tupleInstance = ["bool"];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(true);
+
+        tupleInstance = ["bool", true];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(true);
+      });
+
+      it("rejects tuples not matching parent", () => {
+        // @ts-expect-error: Second item cannot exist (should be bool AND number)
+        tupleInstance = ["num", 42];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(false);
+      });
+    });
+
+    describe("Open tuples (3)", () => {
+      const tupleSchema = {
+        type: "array",
+        items: [{ type: "string" }],
+        additionalItems: { type: "boolean" },
+        oneOf: [
+          {
+            items: [{ type: "string" }],
+            additionalItems: { type: "number" },
+          },
+        ],
+      } as const;
+
+      type Tuple = FromSchema<typeof tupleSchema>;
+      let tupleInstance: Tuple;
+
+      it("accepts tuples matching parent and child", () => {
+        tupleInstance = ["str"];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(true);
+      });
+
+      it("rejects tuples with additional items as child and parent don't match", () => {
+        // @ts-expect-error: Second item cannot exist (should be bool AND number)
+        tupleInstance = ["num", 42];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(false);
+
+        // @ts-expect-error: Second item cannot exist (should be bool AND number)
+        tupleInstance = ["num", true];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(false);
+      });
+    });
+
+    describe("Closed (1) to open tuple", () => {
+      const tupleSchema = {
+        type: "array",
+        items: [{ type: "string" }],
+        oneOf: [
+          { items: [{ const: "stop" }], additionalItems: false },
+          { items: [{ const: "continue" }] },
+        ],
+      } as const;
+
+      type Tuple = FromSchema<typeof tupleSchema>;
+      let tupleInstance: Tuple;
+
+      it("accepts tuples matching #1", () => {
+        tupleInstance = ["stop"];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(true);
+      });
+
+      it("accepts tuples matching #2", () => {
+        tupleInstance = ["continue"];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(true);
+
+        tupleInstance = ["continue", { any: "value" }];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(true);
+      });
+
+      it("rejects tuples matching neither", () => {
+        // @ts-expect-error: First item should be "stop"/"continue"
+        tupleInstance = ["invalid value"];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(false);
+
+        // @ts-expect-error: Second item is denied
+        tupleInstance = ["stop", { any: "value" }];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(false);
+      });
+    });
+
+    describe("Closed (2) to open tuple", () => {
+      const tupleSchema = {
+        type: "array",
+        items: [{ type: "string" }],
+        oneOf: [
+          {
+            items: [{ const: "num" }, { type: "number" }],
+            additionalItems: false,
+            minItems: 2,
+          },
+          {
+            items: [{ const: "bool" }, { type: "boolean" }],
+            additionalItems: false,
+            minItems: 1,
+          },
+        ],
+      } as const;
+
+      type FactoredTuple = FromSchema<typeof tupleSchema>;
+      let tupleInstance: FactoredTuple;
+
+      it("accepts tuples matching #1", () => {
+        tupleInstance = ["num", 42];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(true);
+      });
+
+      it("accepts tuples matching #2", () => {
+        tupleInstance = ["bool"];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(true);
+
+        tupleInstance = ["bool", true];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(true);
+      });
+
+      it("rejects tuples matching neither", () => {
+        // @ts-expect-error: Third item is denied
+        tupleInstance = ["num", 42, "additional item"];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(false);
+
+        // @ts-expect-error: Third item is denied
+        tupleInstance = ["bool", true, "additional item"];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(false);
+      });
+    });
+
+    describe("Closed to open tuple (impossible 1)", () => {
+      const tupleSchema = {
+        type: "array",
+        items: [{ type: "number" }, { type: "number" }],
+        minItems: 2,
+        oneOf: [{ items: [{ type: "number" }], additionalItems: false }],
+      } as const;
+
+      type FactoredTuple = FromSchema<typeof tupleSchema>;
+      let tupleInstance: FactoredTuple;
+
+      it("rejects tuple matching child schema as parent requires 2 items", () => {
+        // @ts-expect-error
+        tupleInstance = [0];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(false);
+      });
+
+      it("rejects tuple matching parent schema as child is closed", () => {
+        // @ts-expect-error
+        tupleInstance = [0, 1];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(false);
+      });
+    });
+
+    describe("Closed to open tuple (impossible 2)", () => {
+      const tupleSchema = {
+        type: "array",
+        items: [{ type: "string" }, { type: "boolean" }],
+        minItems: 2,
+        oneOf: [
+          {
+            items: [{ type: "string" }, { type: "number" }],
+            additionalItems: false,
+          },
+        ],
+      } as const;
+
+      type FactoredTuple = FromSchema<typeof tupleSchema>;
+      let tupleInstance: FactoredTuple;
+
+      it("rejects non-boolean second item (required by parent)", () => {
+        // @ts-expect-error
+        tupleInstance = ["string", 42];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(false);
+      });
+
+      it("rejects non-number second item (required by child)", () => {
+        // @ts-expect-error
+        tupleInstance = ["string", true];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(false);
+      });
+    });
+
+    describe("Open (1) to closed tuple", () => {
+      const tupleSchema = {
+        type: "array",
+        items: [{ type: "string" }],
+        minItems: 1,
+        additionalItems: false,
+        oneOf: [
+          { items: [{ const: "apple" }] },
+          { items: [{ enum: ["tomato", "banana"] }] },
+        ],
+      } as const;
+
+      type FactoredTuple = FromSchema<typeof tupleSchema>;
+      let tupleInstance: FactoredTuple;
+
+      it("accepts valid tuple", () => {
+        tupleInstance = ["apple"];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(true);
+
+        tupleInstance = ["tomato"];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(true);
+
+        tupleInstance = ["banana"];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(true);
+      });
+
+      it("rejects invalid tuple", () => {
+        // @ts-expect-error: One item is required by parent
+        tupleInstance = [];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(false);
+
+        // @ts-expect-error: Additional items are denied
+        tupleInstance = ["apple", "tomato"];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(false);
+      });
+    });
+
+    describe("Open (2) to closed tuple", () => {
+      const tupleSchema = {
+        type: "array",
+        items: [{ type: "string" }],
+        additionalItems: false,
+        oneOf: [
+          { items: [{ const: "several" }, { const: "items" }], minItems: 2 },
+          { items: [{ const: "only one" }] },
+        ],
+      } as const;
+
+      type FactoredTuple = FromSchema<typeof tupleSchema>;
+      let tupleInstance: FactoredTuple;
+
+      it("accepts tuples matching #2", () => {
+        tupleInstance = [];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(true);
+
+        tupleInstance = ["only one"];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(true);
+      });
+
+      it("rejects tuples matching #1 as parent is closed", () => {
+        // @ts-expect-error: Second item is not allowed as additionalProperty
+        tupleInstance = ["several", "items"];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(false);
+
+        // @ts-expect-error: second item is missing
+        tupleInstance = ["several"];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(false);
+      });
+    });
+
+    describe("Open (3) to closed tuple", () => {
+      const tupleSchema = {
+        type: "array",
+        items: [{ type: "string" }, { type: "boolean" }],
+        minItems: 2,
+        additionalItems: false,
+        oneOf: [{ items: [{ const: "can have additionalItems" }] }],
+      } as const;
+
+      type FactoredTuple = FromSchema<typeof tupleSchema>;
+      let tupleInstance: FactoredTuple;
+
+      it("accepts tuples matching parent and child", () => {
+        tupleInstance = ["can have additionalItems", true];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(true);
+      });
+
+      it("rejects invalid tuples", () => {
+        // @ts-expect-error: Second item is required by parent
+        tupleInstance = ["can have additionalItems"];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(false);
+
+        // @ts-expect-error: First item should be child's const
+        tupleInstance = ["no child const"];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(false);
+      });
+    });
+
+    describe("Open to closed tuple (impossible)", () => {
+      const factoredTupleSchema = {
+        type: "array",
+        items: [{ type: "string" }, { type: "boolean" }],
+        minItems: 2,
+        additionalItems: false,
+        oneOf: [
+          { items: [{ type: "string" }], additionalItems: { type: "number" } },
+        ],
+      } as const;
+
+      type FactoredTuple = FromSchema<typeof factoredTupleSchema>;
+      let tupleInstance: FactoredTuple;
+
+      it("rejects tuple having number second item as parent requires boolean", () => {
+        // @ts-expect-error
+        tupleInstance = ["str", 42];
+        expect(ajv.validate(factoredTupleSchema, tupleInstance)).toBe(false);
+      });
+
+      it("rejects tuple having boolean second item as child requires number", () => {
+        // @ts-expect-error
+        tupleInstance = ["str", true];
+        expect(ajv.validate(factoredTupleSchema, tupleInstance)).toBe(false);
+      });
+
+      it("rejects object not having second items as it is required by parent", () => {
+        // @ts-expect-error
+        tupleInstance = ["str"];
+        expect(ajv.validate(factoredTupleSchema, tupleInstance)).toBe(false);
+      });
+    });
+
+    describe("Incorrect schema", () => {
+      const tupleSchema = {
+        type: "array",
+        items: [{ type: "string" }],
+        oneOf: [{ additionalItems: { type: "boolean" } }],
+      } as const;
+
+      type FactoredTuple = FromSchema<typeof tupleSchema>;
+      let tupleInstance: FactoredTuple;
+
+      it("accepts tuples with any additional items", () => {
+        tupleInstance = [
+          "can have additionalItems",
+          'yes, because "items" is missing in oneOf schema',
+        ];
+        expect(ajv.validate(tupleSchema, tupleInstance)).toBe(true);
+      });
+    });
+
+    describe("Min/max items", () => {
+      const factoredTupleSchema = {
+        type: "array",
+        items: [{ type: "string" }, { type: "number" }, { type: "boolean" }],
+        oneOf: [{ minItems: 3 }, { maxItems: 1 }],
+      } as const;
+
+      type FactoredTuple = FromSchema<typeof factoredTupleSchema>;
+      let factoredTupleInstance: FactoredTuple;
+
+      it("accepts tuples with <= 1 items", () => {
+        factoredTupleInstance = [];
+        expect(ajv.validate(factoredTupleSchema, factoredTupleInstance)).toBe(
+          true
+        );
+
+        factoredTupleInstance = ["0"];
+        expect(ajv.validate(factoredTupleSchema, factoredTupleInstance)).toBe(
+          true
+        );
+      });
+
+      it("accepts tuples with >= 3 items", () => {
+        factoredTupleInstance = ["0", 1, true];
+        expect(ajv.validate(factoredTupleSchema, factoredTupleInstance)).toBe(
+          true
+        );
+
+        factoredTupleInstance = ["0", 1, true, "any"];
+        expect(ajv.validate(factoredTupleSchema, factoredTupleInstance)).toBe(
+          true
+        );
+      });
+
+      it("rejects tuples with 2 items", () => {
+        // @ts-expect-error: Tuples should not have 2 items
+        factoredTupleInstance = ["0", 1];
+        expect(ajv.validate(factoredTupleSchema, factoredTupleInstance)).toBe(
+          false
+        );
+      });
+    });
+  });
 });
