c0743d09a04847d35d7513d18e2af42f935d1b2c
ThomasAribart <thomasa@theodo.fr>
2021-05-25 13:53:14 +0200

    remove filter extending

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,5 @@
 export { And } from "./and";
 export { DoesExtend, IsObject, IsArray } from "./extends";
-export { FilterExtending } from "./filter";
 export { Get, DeepGet } from "./get";
 export { HasKeyIn } from "./hasKeyIn";
 export { DeepMergeSafe, DeepMergeUnsafe, Merge } from "./merge";
