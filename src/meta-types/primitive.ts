80556d719f830f6eb4b2c28d59ed1eb7ee72c142
vikas gupta <vikas.rg@somaiya.edu>
2021-08-05 15:30:56 +0530

    Update primitive.ts

--- src/meta-types/primitive.ts
+++ src/meta-types/primitive.ts
@@ -4,7 +4,7 @@ export type PrimitiveType = "primitive";
 
 export type Primitive<L, S> = {
   type: PrimitiveType;
-  value: L | (Get<S, 'nullable', null> extends true ? null : never)
+  value: L | (Get<S, 'nullable', null> extends true ? null : never);
 };
 
 export type Value<L> = Get<L, "value">;
