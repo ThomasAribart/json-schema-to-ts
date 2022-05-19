95fef8f46c3cbbd9181fe0a7e87f5bb1f474544d
ThomasAribart <thomasa@theodo.fr>
2022-05-20 00:39:22 +0200

    improve validator and compiler helpers definition

--- src/type-guards/compiler.ts
+++ src/type-guards/compiler.ts
@@ -7,11 +7,17 @@ import {
 
 export type Compiler = (schema: JSONSchema) => (data: unknown) => boolean;
 
-export const wrapCompilerAsTypeGuard =
+type CompilerWrapper = <O extends FromSchemaOptions = FromSchemaDefaultOptions>(
+  compiler: Compiler
+) => <S extends JSONSchema, T = FromSchema<S, O>>(
+  schema: S
+) => (data: unknown) => data is T;
+
+export const wrapCompilerAsTypeGuard: CompilerWrapper =
   <O extends FromSchemaOptions = FromSchemaDefaultOptions>(
-    validatorBuilder: Compiler
+    compiler: Compiler
   ) =>
   <S extends JSONSchema, T = FromSchema<S, O>>(schema: S) => {
-    const validator = validatorBuilder(schema);
+    const validator = compiler(schema);
     return (data: unknown): data is T => validator(data);
   };
