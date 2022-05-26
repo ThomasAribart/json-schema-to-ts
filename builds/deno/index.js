f8881969f1d99e702c0dfb6616ee1b058c156b69
ThomasAribart <thomasa@theodo.fr>
2022-05-26 16:33:34 +0200

    v2.5.2-beta.0 release

---
+++ builds/deno/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var wrapCompilerAsTypeGuard = function (compiler) {
+    return function (schema) {
+        var validator = compiler(schema);
+        return function (data) { return validator(data); };
+    };
+};
+
+var wrapValidatorAsTypeGuard = function (validator) {
+    return function (schema, data) {
+        return validator(schema, data);
+    };
+};
+
+exports.wrapCompilerAsTypeGuard = wrapCompilerAsTypeGuard;
+exports.wrapValidatorAsTypeGuard = wrapValidatorAsTypeGuard;
---
+++ builds/deno/index.mjs
@@ -0,0 +1,14 @@
+var wrapCompilerAsTypeGuard = function (compiler) {
+    return function (schema) {
+        var validator = compiler(schema);
+        return function (data) { return validator(data); };
+    };
+};
+
+var wrapValidatorAsTypeGuard = function (validator) {
+    return function (schema, data) {
+        return validator(schema, data);
+    };
+};
+
+export { wrapCompilerAsTypeGuard, wrapValidatorAsTypeGuard };
