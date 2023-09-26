c0597f83d14709814384b1d3aef911b1619c75a4
ThomasAribart <thomasa@theodo.fr>
2023-09-26 23:38:10 +0200

    simplify const schemas parsing

--- src/parse-schema/const.ts
+++ src/parse-schema/const.ts
@@ -3,8 +3,6 @@ import type { M } from "ts-algebra";
 import type { JSONSchema7 } from "~/definitions";
 
 import type { ParseSchema, ParseSchemaOptions } from "./index";
-import type { MultipleTypesSchema } from "./multipleTypes";
-import type { SingleTypeSchema } from "./singleType";
 
 /**
  * JSON schemas of constants (i.e. types with cardinalities of 1)
@@ -26,22 +24,6 @@ export type ConstSchema = JSONSchema7 & { const: unknown };
 export type ParseConstSchema<
   CONST_SCHEMA extends ConstSchema,
   OPTIONS extends ParseSchemaOptions,
-> = CONST_SCHEMA extends SingleTypeSchema
-  ? IntersectConstAndTypeSchema<CONST_SCHEMA, OPTIONS>
-  : CONST_SCHEMA extends MultipleTypesSchema
-  ? IntersectConstAndTypeSchema<CONST_SCHEMA, OPTIONS>
-  : ParseConst<CONST_SCHEMA>;
-
-/**
- * Intersects the parsed meta-type of a constant JSON schema to the rest of the schema (only when a `type` keyword has been used).
- * @param CONST_SCHEMA JSONSchema (constant & single/multiple type)
- * @param OPTIONS Parsing options
- * @returns Meta-type
- */
-type IntersectConstAndTypeSchema<
-  CONST_SCHEMA extends ConstSchema & (SingleTypeSchema | MultipleTypesSchema),
-  OPTIONS extends ParseSchemaOptions,
-  // TOIMPROVE: Directly use ParseMultipleTypeSchema and ParseSingleTypeSchema
 > = M.$Intersect<
   ParseConst<CONST_SCHEMA>,
   ParseSchema<Omit<CONST_SCHEMA, "const">, OPTIONS>
